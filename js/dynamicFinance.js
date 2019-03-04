02abc9f2889402da66496f94b3e1fb2f09444276
Joseph Goulette <47185604+gewey@users.noreply.github.com>
2019-03-04 12:31:01 -0500

    dynamicFinance 2

--- js/dynamicFinance.js
+++ js/dynamicFinance.js
@@ -3,5 +3,8 @@ var financeBookmarks
     = "<a title='Paypal' target='_parent'href='https://www.paypal.com/'><img id='bookmark' alt='paypal'></a>"
     + "<a title='E*Trade'target='_parent'href='https://us.etrade.com/home' ><img id='bookmark' alt='etrade'></a>"
     + "<a title='Chase'target='_parent'href='https://www.chase.com/' ><img id='bookmark' alt='chase'></a>"
-    + "<a title='Bank of America'target='_parent'href='https://www.bankofamerica.com/' ><img id='bookmark' alt='bankofamerica'></a>";
+    + "<a title='Bank of America'target='_parent'href='https://www.bankofamerica.com/' ><img id='bookmark' alt='bankofamerica'></a>"
+    + "<a title='ShutterStock Affil' target='_parent'href='https://submit.shutterstock.com/?ref=224491653' ><img id='bookmark' alt='ShutterstockReq'></a>"
+    + "<a title='Amazon Associate' target='_parent'href='https://affiliate-program.amazon.com/home'><img id='bookmark' alt='AmazonAssociate'></a>"
+    + "<a title='Ad Fly' target='_parent'href='https://join-adf.ly/2964346'><img id='bookmark' alt='adfly'></a>";
 document.body.innerHTML += financeBookmarks;
