81031ae3472305130533d8dc9dc7fc050893a85d
Joseph Goulette <47185604+gewey@users.noreply.github.com>
2019-03-04 12:17:33 -0500

    dynamicShopping

---
+++ js/dynamicFinance.js
@@ -0,0 +1,7 @@
+
+
+
+<a title='Paypal' target='_parent'href='https://www.paypal.com/'><img id='bookmark' alt='paypal'></a>
+<a title='E*Trade'target='_parent'href='https://us.etrade.com/home' ><img id='bookmark' alt='etrade'></a>
+<a title='Chase'target='_parent'href='https://www.chase.com/' ><img id='bookmark' alt='chase'></a>
+<a title='Bank of America'target='_parent'href='https://www.bankofamerica.com/' ><img id='bookmark' alt='bankofamerica'></a>
