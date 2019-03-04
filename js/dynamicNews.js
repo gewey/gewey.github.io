ea822046720077af9867b6a216f8ae3fc1dc046e
Joseph Goulette <47185604+gewey@users.noreply.github.com>
2019-03-04 12:46:09 -0500

    dynamicNews & iSports

---
+++ js/dynamicNews.js
@@ -0,0 +1,12 @@
+var document;
+var emailBookmarks
+    = "<a title='CNN'target='_parent'href='http://www.cnn.com/'><img id='bookmark' alt='cnn'></a>"
+    + "<a title='Huffington Post'target='_parent'href='http://www.huffingtonpost.com/' ><img id='bookmark' alt='huffpost'></a>"
+    + "<a title='New York Times' target='_parent'href='http://www.nytimes.com/'><img id='bookmark' alt='nytimes'></a>"
+    + "<a title='BBC'target='_parent'href='http://www.bbc.com/'><img id='bookmark' alt='bbc'></a>"
+    + "<a title='Weather'target='_parent'href='http://www.weather.com/'><img id='bookmark' alt='weatherchannel'></a>"
+    + "<a title='NASA' target='_parent'href='http://www.nasa.gov/' ><img id='bookmark' alt='nasa'></a>"
+    + "<a title='Science Channel'target='_parent'href='https://www.sciencechannel.com/'><img id='bookmark' alt='ScienceChannel'></a>"
+    + "<a title='cnet' target='_parent'href='http://www.cnet.com/' ><img id='bookmark' alt='cnet'></a>"
+    + "<a title='Global Citizen' target='_parent'href='https://www.globalcitizen.org/en/#' ><img id='bookmark' alt='glblctzn'></a>";
+document.body.innerHTML += emailBookmarks;
