8ec2cbf7fa45f4553f1a61a085fa828081f56865
Joseph Goulette <47185604+gewey@users.noreply.github.com>
2019-03-04 13:42:25 -0500

    dynamicDownloads
    and added a cloud nav button

---
+++ js/dynamicDownloads.js
@@ -0,0 +1,29 @@
+var document;
+var socialBookmarks
+    = "<a title='Google Chrome'target='_parent'href='https://www.google.com/chrome/' ><img id='bookmark' alt='googlechrome'></a>"
+    + "<a title='AVG'target='_parent'href='http://www.avg.com/us-en/homepage'><img id='bookmark' alt='avgfree'></a>"
+    + "<a title='Microsoft'target='_parent'href='http://www.microsoft.com/'><img id='bookmark' alt='microsoft'></a>"
+    + "<a title='Direct X' target='_parent'href='http://www.microsoft.com/en-us/download/details.aspx?id=35'><img id='bookmark' alt='directx12' /></a>"
+    + "<a title='Apple'target='_parent'href='http://www.apple.com/'><img id='bookmark' alt='apple'></a>"
+    + "<a title='Ubuntu' target='_parent'href='https://www.ubuntu.com' ><img id='bookmark' alt='ubuntu'></a>"
+    + "<a title='Nvidia' target='_parent'href='https://www.nvidia.com/en-us/'><img id='bookmark' alt='nvidia'></a>"
+    + "<a title='Ninite' target='_parent'href='https://ninite.com/'><img id='bookmark' alt='ninite'></a>"
+    + "<a title='uTorrent' target='_parent'href='https://www.utorrent.com/'><img id='bookmark' alt='utorrent'></a>"
+    + "<a title='Java' target='_parent'href='http://java.com/en/download/index.jsp'><img id='bookmark' alt='java' /></a>"
+    + "<a title='Adobe Reader' target='_parent'href='http://get.adobe.com/reader/' ><img id='bookmark' alt='acrobat' /></a>"
+    + "<a title='Adobe Air'target='_parent'href='http://get.adobe.com/air/'><img id='bookmark' alt='air_devs' /></a>"
+    + "<a title='Adobe Flash'target='_parent'href='http://get.adobe.com/flashplayer/'><img id='bookmark' alt='adobeflash' /></a>";
+document.body.innerHTML += socialBookmarks;
+
+
+
+
+
+
+
+
+
+
+
+
+
