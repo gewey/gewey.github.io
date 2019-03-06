546f215ee6a50a7a836edce76b39c66962bdbf8a
Joseph Goulette <47185604+gewey@users.noreply.github.com>
2019-03-06 09:24:44 -0500

    Update dynamicFinance.js

--- js/dynamicFinance.js
+++ js/dynamicFinance.js
@@ -4,6 +4,7 @@ let financeBookmarks
     + "<a title='E*Trade'target='_parent'href='https://us.etrade.com/home' ><img id='bookmark' alt='etrade'></a>"
     + "<a title='Chase'target='_parent'href='https://www.chase.com/' ><img id='bookmark' alt='chase'></a>"
     + "<a title='Bank of America'target='_parent'href='https://www.bankofamerica.com/' ><img id='bookmark' alt='bankofamerica'></a>"
+    + "<a title='Coinbase' target='_parent' href='https://www.coinbase.com/' ><img id='bookmark' alt='coinbase'></a>"
     + "<a title='ShutterStock Affil' target='_parent'href='https://submit.shutterstock.com/?ref=224491653' ><img id='bookmark' alt='ShutterstockReq'></a>"
     + "<a title='Amazon Associate' target='_parent'href='https://affiliate-program.amazon.com/home'><img id='bookmark' alt='AmazonAssociate'></a>"
     + "<a title='Ad Fly' target='_parent'href='https://join-adf.ly/2964346'><img id='bookmark' alt='adfly'></a>";
