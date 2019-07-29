3212ff6f97af14b78bc4298a0f01590e166d6ea1
Joseph Goulette <47185604+gewey@users.noreply.github.com>
2019-07-28 21:53:30 -0400

    Update dynamicNews.js
    last week tonight

--- js/dynamicNews.js
+++ js/dynamicNews.js
@@ -5,7 +5,8 @@ let newsBookmarks
     + "<a title='BBC'target='_parent'href='http://www.bbc.com/'><img id='bookmark' alt='bbc'></a>"
     + "<a title='Weather'target='_parent'href='http://www.weather.com/'><img id='bookmark' alt='weatherchannel'></a>"
     + "<a title='NASA' target='_parent'href='http://www.nasa.gov/' ><img id='bookmark' alt='nasa'></a>"
-    + "<a title='Science Channel'target='_parent'href='https://www.sciencechannel.com/'><img id='bookmark' alt='ScienceChannel'></a>"
+    + "<a title='Science Channel'target='_parent'href='https://www.sciencechannel.com'><img id='bookmark' alt='ScienceChannel'></a>"
     + "<a title='cnet' target='_parent'href='http://www.cnet.com/' ><img id='bookmark' alt='cnet'></a>"
-    + "<a title='Global Citizen' target='_parent'href='https://www.globalcitizen.org/en/#' ><img id='bookmark' alt='glblctzn'></a>";
+    + "<a title='Last Week Tonight' target='_parent'href='https://www.youtube.com/user/LastWeekTonight' ><img id='bookmark' alt='LastWeekTonight'></a>"
+    + "<a title='Global Citizen' target='_parent'href='https://www.globalcitizen.org' ><img id='bookmark' alt='glblctzn'></a>";
 document.body.innerHTML += newsBookmarks;
