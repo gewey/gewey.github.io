f408cb89747bfbd3d8bb04740de53ed7e647382a
Joseph Goulette <47185604+gewey@users.noreply.github.com>
2019-07-29 20:14:11 -0400

    Update dynamicEmail.js
    test

--- js/dynamicEmail.js
+++ js/dynamicEmail.js
@@ -1,5 +1,6 @@
 let emailBookmarks
-    = "<a title='Gmail'target='_parent'href='https://mail.google.com/' ><img id='bookmark' alt='gmail'></a>"
+    = "<br />"
+    + "<a title='Gmail'target='_parent'href='https://mail.google.com/' ><img id='bookmark' alt='gmail'></a>"
     + "<a title='Hotmail'target='_parent'href='https://outlook.live.com' ><img id='bookmark' alt='outlook'></a>"
     + "<a title='Yahoo![HTTPS]'target='_parent'href='https://www.yahoo.com/' ><img id='bookmark' alt='yahoo'></a>"
     + "<a title='AOL [HTTPS]'target='_parent'href='https://www.aol.com/' ><img id='bookmark' alt='aol'></a>";
