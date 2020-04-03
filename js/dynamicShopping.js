c2ae48e9b24af865580966ae078faad6e8d99f34
Joseph Goulette <47185604+gewey@users.noreply.github.com>
2020-04-03 16:12:28 -0400

    Update dynamicShopping.js
    added misfit markets

--- js/dynamicShopping.js
+++ js/dynamicShopping.js
@@ -5,6 +5,7 @@ let shoppingBookmarks
     + "<a title='Jinx' target='_parent'href='http://www.jinx.com/member/336691?bid=49' ><img id='bookmark' alt='jinx'></a>"
     + "<a title='Newegg' target='_parent'href='http://www.newegg.com/' ><img id='bookmark' alt='newegg'></a>"
     + "<a title='Amazon' target='_parent'href='https://www.amazon.com?&_encoding=UTF8&tag=gewey-20&linkCode=ur2&linkId=0c48ef8dc6e3009ccbaad420f071e1c5&camp=1789&creative=9325'><img id='bookmark' alt='amazon'></a>"
+    + "<a title='Misfits Market' target='_parent'https://www.misfitsmarket.com/' ><img id='bookmark' alt='misfitsmarket'></a>"
     + "<a title='Craigslist' target='_parent'href='http://www.craigslist.org/' ><img id='bookmark' alt='craigslist'></a>"
     + "<a title='Wal Mart' target='_parent'href='http://www.walmart.com/'><img id='bookmark' alt='walmart'></a>"
     + "<a title='UPS'target='_parent'href='http://www.ups.com/'><img id='bookmark' alt='ups'></a>"
