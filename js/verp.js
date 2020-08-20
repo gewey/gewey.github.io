313d530a9a52e00509375a4966e1c1663a082241
Joseph Goulette <47185604+gewey@users.noreply.github.com>
2020-08-20 15:46:32 -0400

    Update verp.js

--- js/verp.js
+++ js/verp.js
@@ -6,6 +6,8 @@ let verp = "";
 for (iC = 0; iC < images.length; iC++) {
     verp = images[iC].getAttribute("alt");
     images[iC].setAttribute("src", "https://avatars.io/twitter/" + verp + "/256/");
+    image.onerror = "";
+    image.src = "/img/btn/bm/" + verp + ".png";
     txt = txt +  images[iC].alt + "___" + images[iC].src + "<br>";
 }
 function imgError(image) {
