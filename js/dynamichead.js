7f3fef97e1c012fd764d541ce81fcba139b05a40
Joseph Goulette <47185604+gewey@users.noreply.github.com>
2020-09-24 02:54:51 -0400

    wowhead tooltips
    added support for wowhead tooltips

--- js/dynamichead.js
+++ js/dynamichead.js
@@ -14,10 +14,11 @@ let theHead
     + "<link rel='stylesheet' href='https://gewey.github.io/css/bones.css' type='text/css'>"
     + "<link rel='stylesheet' href='https://gewey.github.io/css/text.css' type='text/css'>"
     + "<link rel='stylesheet' href='https://gewey.github.io/css/images.css' type='text/css'>"
-    + "<script src='http://www.wowhead.com/widgets/power.js'></script>"
+    + "<script type='text/javascript' src='http://wow.zamimg.com/widgets/newsfeed.js'></script>"
+    + "<script>const whTooltips = {colorLinks: true, iconizeLinks: true, renameLinks: true};</script>"
+    + "<script src='https://wow.zamimg.com/widgets/power.js'></script>"
     + "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>"
     + "<script src='https://gewey.github.io/js/google_analytics.js'></script>";
-
 
 //window.onload = function() {
 //   document.getElementsByTagName("head")[0].innerHTML = theHead;
