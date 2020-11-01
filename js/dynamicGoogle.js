a080a872662d8cff16e2e2299beadf6615aafbaf
Joseph Goulette <47185604+gewey@users.noreply.github.com>
2020-11-01 11:41:39 -0500

    forge

--- js/dynamicGoogle.js
+++ js/dynamicGoogle.js
@@ -7,6 +7,7 @@ let googleBookmarks
     + "<a title='Gmail'target='_parent'href='https://mail.google.com/' ><img id='bookmark' alt='gmail'></a>"
     + "<a title='Google Photos'     target='_parent'href='https://photos.google.com/'                   ><img id='bookmark' alt='googlephotos'      ></a>"
 
+    + "<a title='Google Translate' target='_parent'href='https://translate.google.com/'><img id='bookmark' alt='googletranslate'></a>"
 
 
 
