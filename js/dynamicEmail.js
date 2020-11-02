17540c4af951636ca52e0966c21e8db9875c0a0d
Joseph Goulette <47185604+gewey@users.noreply.github.com>
2020-11-01 22:02:26 -0500

    email

--- js/dynamicEmail.js
+++ js/dynamicEmail.js
@@ -1,5 +1,5 @@
 let emailBookmarks
-    = "<a title='Gewey Referral'    target='contentframe'href='./shopping/referral.html'                    ><img id='bookmark' alt='blank'></a>"
+    = "<a title='Email'    target='contentframe'href='./email/index.html'                    ><img id='bookmark' alt='email'></a>"
     + "<a title='Hotmail'target='_parent'href='https://outlook.live.com' ><img id='bookmark' alt='outlook'></a>"
     + "<a title='Yahoo!Mail[HTTPS]'target='_parent'href='https://mail.yahoo.com/' ><img id='bookmark' alt='yahoomail'></a>";
 document.body.innerHTML += emailBookmarks;
