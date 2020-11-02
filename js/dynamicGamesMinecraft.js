12743a5f995a3b664d4a3dcd3e1a33f4cca0b4f0
Joseph Goulette <47185604+gewey@users.noreply.github.com>
2020-11-02 13:14:41 -0500

    sep games

---
+++ js/dynamicGamesMinecraft.js
@@ -0,0 +1,11 @@
+let gamesminecraftBookmarks
+    = "<a title='Games'    target='contentframe'href='./games/index.html'                    ><img id='bookmark' alt='game'></a>"
+    + "<a title='minecraft'target='_parent'href='https://minecraft.net/' ><img id='bookmark' alt='minecraft'></a>"
+    + "<a title='minecraft wiki'target='_parent'https://minecraft.gamepedia.com/Minecraft_Wiki' ><img id='bookmark' alt='minecraftwiki'></a>"
+    + "<a title='Forge'target='_parent'href='https://files.minecraftforge.net/'><img id='bookmark' alt='ForgeDevTeam'></a>"
+    + "<a title='FTBWiki'target='_parent'href='https://ftb.gamepedia.com/FTB_Wiki/'><img id='bookmark' alt='ftb_team'></a>"
+    + "<a title='MCREATOR' target='_parent'href='https://mcreator.net/'><img id='bookmark' alt='PyloDEV'></a>"
+    + "<a title='Crafatar' target='_parent'href='https://crafatar.com/'><img id='bookmark' alt='Crafatar'></a>";
+document.body.innerHTML += gamesminecraftBookmarks;
+
+
---
+++ js/dynamicGamesRunescape.js
@@ -0,0 +1,10 @@
+let gamesrunescapeBookmarks
+    = "<a title='Games'    target='contentframe'href='./games/index.html'                    ><img id='bookmark' alt='game'></a>"
+    + "<a title='Runescape'target='_parent'href='http://runescape.com/'><img id='bookmark' alt='runescape'></a>"
+    + "<a title='Old SChool RS'target='_parent'href='https://oldschool.runescape.com'><img id='bookmark' alt='oldschoolrs'></a>"
+    + "<a title='Rune Wiki'target='_parent'href='https://runescape.wiki/'><img id='bookmark' alt='RSWiki'></a>"
+    + "<a title='Rune Tips'target='_parent'href='https://www.tip.it/runescape/'><img id='bookmark' alt='runetips'></a>"
+    + "<a title='Rune HQ'target='_parent'href='https://www.runehq.com' ><img id='bookmark' alt='officialrunehq'></a>";
+document.body.innerHTML += gamesrunescapeBookmarks;
+
+
