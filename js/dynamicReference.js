a04efb8eeb7f3e65dc217f3355b017ca67b07e2c
Joseph Goulette <47185604+gewey@users.noreply.github.com>
2020-11-21 14:17:28 -0500

    ancestry

--- js/dynamicReference.js
+++ js/dynamicReference.js
@@ -1,5 +1,7 @@
 let referenceBookmarks
-    = "<a title='Reference'    target='contentframe'href='./reference/index.html'                    ><img id='bookmark' alt='reference'></a>"
+    = "<a title='Reference'     target='contentframe'   href='./reference/index.html'                   ><img id='bookmark' alt='reference'></a>"
+    + "<a title='Ancestry'      target='_parent'        href='https://www.ancestry.com/'                ><img id='bookmark' alt='Ancestry'></a>"
+    + "<a title='23 & Me'       target='_parent'        href='https://www.23andme.com/'                 ><img id='bookmark' alt='23andMe'></a>"
     + "<a title='Wikipedia'target='_parent'href='http://wikipedia.org/'><img id='bookmark' alt='wikipedia'></a>"
     + "<a title='IMDB' target='_parent'href='http://www.imdb.com/' ><img id='bookmark' alt='imdb'></a>"
     + "<a title='ehow' target='_parent'href='http://www.ehow.com/' ><img id='bookmark' alt='ehow'></a>"
