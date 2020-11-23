ea07653f5688d7b44a691e9728fc247f627dd4af
Joseph Goulette <47185604+gewey@users.noreply.github.com>
2020-11-22 19:37:48 -0500

    warcraftclasses

---
+++ js/dynamicGamesWarcraftClasses.js
@@ -0,0 +1,15 @@
+let gameswarcraftclassesBookmarks
+    = "<a title='Mage'    		target='contentframe'	href='./class/mage/index.html'			><img id='bookmark' alt='warcraftmage'></a>"
+    + "<a title='Priest'    	target='contentframe'	href='./class/priest/index.html'		><img id='bookmark' alt='warcraftpriest'></a>"
+    + "<a title='Warlock'		target='contentframe'	href='./class/warlock/index.html'		><img id='bookmark' alt='warcraftwarlock'></a>"
+    + "<a title='Demon Hunter'	target='contentframe'	href='./class/demonhunter/index.html'	><img id='bookmark' alt='warcraftdemonhunter'></a>"
+    + "<a title='Druid'			target='contentframe'	href='./class/druid/index.html'			><img id='bookmark' alt='warcraftdruid'></a>"
+    + "<a title='Monk'			target='contentframe'	href='./class/monk/index.html'			><img id='bookmark' alt='warcraftmonk'></a>"
+    + "<a title='Rogue'			target='contentframe'	href='./class/rogue/index.html'			><img id='bookmark' alt='warcraftrogue'></a>"
+    + "<a title='Hunter'		target='contentframe'	href='./class/hunter/index.html'		><img id='bookmark' alt='warcrafthunter'></a>"
+    + "<a title='Shaman'		target='contentframe'	href='./class/shaman/index.html'		><img id='bookmark' alt='warcraftshaman'></a>"
+    + "<a title='Death Knight'	target='contentframe'	href='./class/deathknight/index.html'	><img id='bookmark' alt='warcraftdeathknight'></a>"
+    + "<a title='Paladin'    	target='contentframe'	href='./class/paladin/index.html'		><img id='bookmark' alt='warcraftpaladin'></a>"
+    + "<a title='Warrior'    	target='contentframe'	href='./class/warrior/index.html'		><img id='bookmark' alt='warcraftwarrior'></a>";
+document.body.innerHTML += gameswarcraftclassesBookmarks;
+
