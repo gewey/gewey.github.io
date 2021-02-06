1146cfc04f4744a69ec4e19abaa18aa7b9f4ce65
gewey <47185604+gewey@users.noreply.github.com>
2021-02-06 16:24:08 -0500

    stonks

---
+++ js/dynamicInvesting.js
@@ -0,0 +1,17 @@
+let financeBookmarks
+    = "<a title='Investing'    	target='contentframe'href='./investing/index.html'                    ><img id='bookmark' alt='investing'></a>"
+    + "<a title='E*Trade'		target='_parent'href='https://us.etrade.com/home' ><img id='bookmark' alt='etrade'></a>"
+    + "<a title='fidelity'     target='_parent' href='https://oltx.fidelity.com/ftgw/fbc/oftop/portfolio#summary' ><img id='bookmark' alt='fidelity'></a>"
+    + "<a title='Robinhood'     target='_parent' href='https://robinhood.com' ><img id='bookmark' alt='RobinhoodApp'></a>"
+    + "<a title='Acorns'        target='_parent' href='https://www.acorns.com/' ><img id='bookmark' alt='acorns'></a>"
+    + "<a title='webull'  target='_parent' href='https://act.webull.com/iv/e5OOcG2gCh7s/71n/inviteUs/' ><img id='bookmark' alt='webull'></a>"
+    + "<a title='ShrimpyApp'  target='_parent' href='https://shrimpy.io/referral?r=9ZfJPAj__' ><img id='bookmark' alt='ShrimpyApp'></a>"
+    + "<a title='binance'       target='_parent' href='https://www.binance.com/en/my/wallet/account/overview' ><img id='bookmark' alt='binance'></a>"
+    + "<a title='BinanceUS'     target='_parent' href='https://www.binance.us/en/usercenter/wallet/balances' ><img id='bookmark' alt='BinanceUS'></a>"
+    + "<a title='AtomicWallet'  target='_parent' href='https://atomicwallet.io/' ><img id='bookmark' alt='AtomicWallet'></a>"
+    + "<a title='Coinbase'      target='_parent' href='https://www.coinbase.com/' ><img id='bookmark' alt='coinbase'></a>"
+    + "<a title='Coinbase'      target='_parent' href='https://www.coinbase.com/' ><img id='bookmark' alt='coinbase'></a>"
+    + "<a title='Coinbase'      target='_parent' href='https://www.coinbase.com/' ><img id='bookmark' alt='coinbase'></a>"
+    + "<a title='Coinbase'      target='_parent' href='https://www.coinbase.com/' ><img id='bookmark' alt='coinbase'></a>"
+    + "<a title='Coinbase'      target='_parent' href='https://www.coinbase.com/' ><img id='bookmark' alt='coinbase'></a>";
+document.body.innerHTML += financeBookmarks;
