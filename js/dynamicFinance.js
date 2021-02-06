183619ed3cba24929043463b25ec8aa0600903ec
gewey <47185604+gewey@users.noreply.github.com>
2021-02-05 23:29:17 -0500

    ShrimpyApp

--- js/dynamicFinance.js
+++ js/dynamicFinance.js
@@ -5,7 +5,7 @@ let financeBookmarks
     + "<a title='Robinhood'     target='_parent' href='https://robinhood.com' ><img id='bookmark' alt='RobinhoodApp'></a>"
     + "<a title='Acorns'        target='_parent' href='https://www.acorns.com/' ><img id='bookmark' alt='acorns'></a>"
     + "<a title='            '  target='_parent' href='                                     ' ><img id='bookmark' alt='                              '></a>"
-    + "<a title='            '  target='_parent' href='                                     ' ><img id='bookmark' alt='                              '></a>"
+    + "<a title='ShrimpyApp'  target='_parent' href='https://shrimpy.io/referral?r=9ZfJPAj__' ><img id='bookmark' alt='ShrimpyApp'></a>"
     + "<a title='binance'       target='_parent' href='https://www.binance.com/en/my/wallet/account/overview' ><img id='bookmark' alt='binance'></a>"
     + "<a title='BinanceUS'     target='_parent' href='https://www.binance.us/en/usercenter/wallet/balances' ><img id='bookmark' alt='BinanceUS'></a>"
     + "<a title='AtomicWallet'  target='_parent' href='https://atomicwallet.io/' ><img id='bookmark' alt='AtomicWallet'></a>"
