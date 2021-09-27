5bf0d41fdbc90a65dba4d400f0ae58571db1089e
Gewey <47185604+gewey@users.noreply.github.com>
2021-09-27 17:04:50 -0400

    crypto

--- js/dynamicCrypto.js
+++ js/dynamicCrypto.js
@@ -1,5 +1,5 @@
 let cryptoBookmarks
-    = "<a title='Crypto'    	target='contentframe'href='./crypto/index.html'                    ><img id='bookmark' alt='crypto'></a>"
+    = "<a title='Crypto'    	target='_parent'./crypto/main.html'                    ><img id='bookmark' alt='crypto'></a>"
     + "<a title='ShrimpyApp'  target='_parent' href='https://shrimpy.io/referral?r=9ZfJPAj__' ><img id='bookmark' alt='ShrimpyApp'></a>"
     + "<a title='binance'       target='_parent' href='https://www.binance.com/en/my/wallet/account/overview' ><img id='bookmark' alt='binance'></a>"
     + "<a title='BinanceUS'     target='_parent' href='https://www.binance.us/en/usercenter/wallet/balances' ><img id='bookmark' alt='BinanceUS'></a>"
