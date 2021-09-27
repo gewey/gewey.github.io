a5edcf951b71793665fcd126a9b7d9f94a015ebe
Gewey <47185604+gewey@users.noreply.github.com>
2021-09-27 16:38:15 -0400

    crypto

---
+++ js/dynamicCrypto.js
@@ -0,0 +1,9 @@
+let investingBookmarks
+    = "<a title='Crypto'    	target='contentframe'href='./crypto/index.html'                    ><img id='bookmark' alt='crypto'></a>"
+    + "<a title='ShrimpyApp'  target='_parent' href='https://shrimpy.io/referral?r=9ZfJPAj__' ><img id='bookmark' alt='ShrimpyApp'></a>"
+    + "<a title='binance'       target='_parent' href='https://www.binance.com/en/my/wallet/account/overview' ><img id='bookmark' alt='binance'></a>"
+    + "<a title='BinanceUS'     target='_parent' href='https://www.binance.us/en/usercenter/wallet/balances' ><img id='bookmark' alt='BinanceUS'></a>"
+    + "<a title='AtomicWallet'  target='_parent' href='https://atomicwallet.io/' ><img id='bookmark' alt='AtomicWallet'></a>"
+    + "<a title='Coinbase'      target='_parent' href='https://www.coinbase.com/join/jgoulette_4hiaeA' ><img id='bookmark' alt='coinbase'></a>"
+    + "<a title='Crypto'    	target='contentframe'href='./crypto/index.html'                    ><img id='bookmark' alt='crypto'></a>";
+document.body.innerHTML += investingBookmarks;
