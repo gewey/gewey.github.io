9cbda792e5445b871ec17736d914f44a55046edf
Viktor <viktor@example.com>
2026-04-15 06:58:20 -0400

    rimworld
    rw

--- .github/agents/PeonAvatar/preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    onPeonEvent: (callback) => ipcRenderer.on('peon-event', (_event, value) => callback(value))
-});
